ae1f6dec7c479d95ec0119aaf2343e8875cff33d
SeekingBlues <seekingblues@gmail.com>
2022-09-12 19:44:45 -0400

    Add mouse odometry proof-of-concept

--- TeamCode/src/main/cpp/lib.cpp
+++ TeamCode/src/main/cpp/lib.cpp
@@ -1,75 +1,94 @@
+#include <android/log.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <jni.h>
 #include <linux/input.h>
+#include <pthread.h>
 #include <string.h>
-#include <type_traits>
 #include <unistd.h>
 
-#define PRINTF(fmt, ...) tprintf(*env, obj, fmt, ##__VA_ARGS__)
+#define LOG(fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, "MouseEvents", fmt, ##__VA_ARGS__)
 
-template<typename... Args>
-void tprintf(JNIEnv& env, jobject obj, const char* fmt, Args&&... args)
+static constexpr auto DEVICE = "/dev/input/event2";
+static pthread_t s_thread;
+
+extern "C" {
+JNIEXPORT void JNICALL Java_org_firstinspires_ftc_teamcode_Driving_MouseTest_mouseStart(JNIEnv*, jobject)
 {
-    auto arr = env.NewObjectArray(sizeof...(args), env.FindClass("java/lang/String"), env.NewStringUTF(""));
-    size_t i = 0;
-    (..., [&] {
-        using T = std::remove_reference_t<Args>;
-        static_assert(std::is_same_v<T, char*> || std::is_integral_v<T>);
-        jobject o = nullptr;
-        if constexpr (std::is_same_v<T, char*>) {
-            o = env.NewStringUTF(args);
-        } else if constexpr (std::is_integral_v<T>) {
-            auto int_class = env.FindClass("java/lang/Integer");
-            o = env.NewObject(int_class, env.GetMethodID(int_class, "<init>", "(I)V"), args);
-        }
+    auto rc = pthread_create(
+        &s_thread, nullptr, [](void*) -> void* {
+            LOG("Trying to open %s", DEVICE);
+            auto fd = open(DEVICE, O_RDONLY);
+            if (fd < 0) {
+                LOG("Failed to open %s: %s", DEVICE, strerror(errno));
+                return nullptr;
+            }
 
-        env.SetObjectArrayElement(arr, i++, o);
-    }());
-    auto klass = env.FindClass("org/firstinspires/ftc/teamcode/Driving/ObjectDetection");
-    auto method = env.GetMethodID(klass, "print", "(Ljava/lang/String;[Ljava/lang/Object;)V");
-    env.CallVoidMethod(obj, method, env.NewStringUTF(fmt), arr);
+            struct sigaction act { };
+            act.sa_handler = [](int) {
+                pthread_exit(nullptr);
+            };
+            auto rc = sigaction(SIGUSR2, &act, nullptr); // SIGUSR1 is blocked by JVM???
+            if (rc < 0) {
+                LOG("Failed to set sigaction: %s", strerror(errno));
+                return nullptr;
+            }
+
+            pthread_cleanup_push([](void* fd) {
+                auto f = *static_cast<int*>(fd);
+                LOG("Closing file descriptor %d", f);
+                close(f);
+            },
+                &fd);
+
+            input_event ev {};
+            int x = 0, y = 0, dx = 0, dy = 0;
+
+            for (;;) {
+                auto rc = read(fd, &ev, sizeof(ev));
+                if (rc < 0) {
+                    LOG("Failed to read: %s", strerror(errno));
+                    break;
+                }
+                if (rc == 0) {
+                    LOG("Reached EOF");
+                    break;
+                }
+
+                if (ev.type == EV_REL) {
+                    if (ev.code == REL_X)
+                        dx += ev.value;
+                    else if (ev.code == REL_Y)
+                        dy += ev.value;
+                    else
+                        LOG("Unknown event code for EV_REL: %d", ev.code);
+                } else if (ev.type == EV_SYN) {
+                    if (ev.code == SYN_REPORT) {
+                        x += dx;
+                        y += dy;
+                        LOG("x = %7d, y = %7d, dx = %4d, dy = %4d", x, y, dx, dy);
+                        dx = dy = 0;
+                    } else {
+                        LOG("Unknown event code for EV_SYN: %d", ev.code);
+                    }
+                } else {
+                    LOG("Unknown event type %d", ev.type);
+                }
+            }
+
+            pthread_cleanup_pop(true);
+            return nullptr;
+        },
+        nullptr);
+    if (rc != 0)
+        LOG("Failed to create thread: %s", strerror(rc));
 }
 
-JNIEXPORT void JNICALL Java_org_firstinspires_ftc_teamcode_Driving_ObjectDetection_dumpEvents(JNIEnv* env, jobject obj)
+JNIEXPORT void JNICALL Java_org_firstinspires_ftc_teamcode_Driving_MouseTest_mouseStop(JNIEnv*, jobject)
 {
-    auto fd = open("/dev/input/event0", O_RDONLY);
-    if (fd < 0) {
-        PRINTF("Failed to open: %s", strerror(errno));
-        return;
-    }
-
-    input_event ev {};
-    int x = 0, y = 0;
-
-    for (;;) {
-        auto rc = read(fd, &ev, sizeof(ev));
-        if (rc < 0) {
-            PRINTF("Failed to read: %s", strerror(errno));
-            break;
-        } else if (rc == 0) {
-            PRINTF("Reached EOF");
-            break;
-        } else {
-            if (ev.type == EV_REL) {
-                if (ev.value == REL_X)
-                    x += ev.value;
-                else if (ev.value == REL_Y)
-                    y += ev.value;
-                else
-                    PRINTF("Unknown value for EV_REL: %d", ev.value);
-            } else if (ev.type == EV_SYN) {
-                if (ev.value == SYN_REPORT) {
-                    PRINTF("%d, %d", x, y);
-                    x = y = 0;
-                } else {
-                    PRINTF("Unknown value for EV_SYN: %d", ev.value);
-                }
-            } else {
-                PRINTF("Unknown type %d", ev.type);
-            }
-        }
-    }
-
-    close(fd);
+    LOG("Stopping thread");
+    auto rc = pthread_kill(s_thread, SIGUSR2);
+    if (rc != 0)
+        LOG("Failed to send signal: %s", strerror(rc));
+}
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving/MouseTest.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.Driving;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Mouse Test", group = "---")
+public class MouseTest extends LinearOpMode {
+    private native void mouseStart();
+
+    private native void mouseStop();
+
+    private void print(Object o) {
+        telemetry.addData("+", o);
+        telemetry.update();
+    }
+
+    private void print(String fmt, Object... o) {
+        telemetry.addData("+", fmt, o);
+        telemetry.update();
+    }
+
+    private void printException(Exception e) {
+        StringWriter writer = new StringWriter();
+        e.printStackTrace(new PrintWriter(writer));
+        print("***** EXCEPTION *****");
+        print(e);
+        print("***** STACK TRACE *****");
+        print(writer.toString());
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        try {
+            telemetry.setAutoClear(false);
+            // We have to run this in adb due to permission error
+            // Runtime.getRuntime().exec("su root chmod 777 /dev/input/*");
+            System.loadLibrary("mouse_events");
+        } catch (Exception e) {
+            printException(e);
+            return;
+        } finally {
+            waitForStart();
+        }
+
+        try {
+            mouseStart();
+        } catch (Exception e) {
+            printException(e);
+        } finally {
+            while (opModeIsActive())
+                ;
+            mouseStop();
+        }
+    }
+}
